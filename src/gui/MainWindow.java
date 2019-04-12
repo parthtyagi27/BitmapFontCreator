7f0bb9e189feceffd10a9a3b325acaacaf6b5bfa
parthtyagi27 <parthtyagi27@users.noreply.github.com>
2019-04-12 13:13:03 -0500

    Added functionality

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,23 +1,35 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application
 {
+    public static final int WIDTH = 750, HEIGHT = 400;
+    public static Stage primaryStage;
+    public static String path;
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        MainWindow.primaryStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("BitmapFontCreator");
+        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
         primaryStage.show();
     }
 
+    public static void setFile()
+    {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Font File");
+        String path = fileChooser.showOpenDialog(primaryStage).getAbsolutePath();
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/gui/MainWindowController.java
+++ src/gui/MainWindowController.java
@@ -1,5 +1,25 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
 public class MainWindowController
 {
+    @FXML
+    private TextField fontFileTextField;
+    @FXML
+    public void pickFontFile()
+    {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Font File");
+        try
+        {
+            MainWindow.path = fileChooser.showOpenDialog(MainWindow.primaryStage).getPath();
+            fontFileTextField.setText(MainWindow.path);
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
