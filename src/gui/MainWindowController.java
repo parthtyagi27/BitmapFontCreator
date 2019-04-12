ab4cc63a8ff890b8190e862980c8d4f694502c63
parthtyagi27 <parthtyagi27@users.noreply.github.com>
2019-04-12 13:38:47 -0500

    Added UI functionality

--- src/gui/MainWindowController.java
+++ src/gui/MainWindowController.java
@@ -1,11 +1,15 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
+import sun.applet.Main;
 
 public class MainWindowController
 {
+    @FXML
+    private TextField fontSizeTextField;
     @FXML
     private TextField fontFileTextField;
     @FXML
@@ -17,6 +21,35 @@ public class MainWindowController
         {
             MainWindow.path = fileChooser.showOpenDialog(MainWindow.primaryStage).getPath();
             fontFileTextField.setText(MainWindow.path);
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    @FXML
+    public void createFont()
+    {
+        try
+        {
+            if(MainWindow.path == "" || MainWindow.path == null)
+            {
+                Alert error = new Alert(Alert.AlertType.ERROR);
+                error.setContentText("Pick a font file");
+                error.show();
+            }else
+            {
+                int size = Integer.parseInt(fontSizeTextField.getText());
+                if (size < 1)
+                {
+                    Alert error = new Alert(Alert.AlertType.ERROR);
+                    error.setContentText("Pick a font size greater than 1");
+                    error.show();
+                } else
+                {
+                    MainWindow.fontSize = size;
+                    MainWindow.generateFont();
+                }
+            }
         }catch (Exception ex)
         {
             ex.printStackTrace();
